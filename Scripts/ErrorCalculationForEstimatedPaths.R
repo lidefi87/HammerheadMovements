3caad64464052d65261e5eec218eccf488051e87
Denisse Fierro Arcos <lidefi87@gmail.com>
2020-05-01 22:43:01 -0500

    Automating process of getting data

--- Scripts/ErrorCalculationForEstimatedPaths.R
+++ Scripts/ErrorCalculationForEstimatedPaths.R
@@ -16,40 +16,58 @@ library(ggforce)
 library(raster)
 library(geosphere)
 
-### Set directory where all data will be saved
-setwd("C:/Users/Denisse/Documents/Manuscripts/miniPAT_SatTags/Spatial/MiniPATs_NewProcess")
+#Get project (current) directory
+MainDir <- getwd()
+#Identify folder where shapefile and csv outputs will be saved
+OutFolder <- "../Spatial/MiniPATs_NewProcess/"
+#Getting a list of file directories of datasets containing HMMoce results
+HMM_results <- list.files(pattern = "HMMoce_results.csv", recursive = T)
 
-# Plotting errors for calculated paths
-## for a 90% confidence interval use threshold = 10
-## see ?getCtr for description of each input but these are smoother, calc.track, and 
-#setup.grid outputs, respectively
-bnds <- getCtr(res$s, res$tr, res$g, threshold = 10, makePlot = F)
+#Create a loop using list of directories to save best results as identified by the model
+for(i in HMM_results){
+  #Load HMMoce result for each tag
+  TagResults <- read.csv(HMM_results[i]) %>% arrange(nll)
+  #Identify best model (first row as dataset has been arranged by nll) and load results
+  #into environment
+  load(list.files(pattern = glob2rx(paste0(TagResults$name[1], "*.rda$")), 
+                  recursive = T))
+  
+  # Plotting errors for calculated paths
+  ## for a 90% confidence interval use threshold = 10
+  ## see ?getCtr for description of each input but these are smoother, calc.track, and 
+  #setup.grid outputs, respectively
+  bnds <- getCtr(res$s, res$tr, res$g, threshold = 10, makePlot = F)
+  
+  ##Converting CIs into a lon and lat error metrics
+  df <- cbind(res$tr, t(data.frame(lapply(bnds, 
+                                          FUN = function(x) c(x$yDist, x$xDist))))) %>% 
+    plyr::rename(c("1" = "ydist", "2" = "xdist")) %>% 
+    ##some contours will likely not calculate so you can fill this in with the mean error 
+    #(or something else) if you wish
+    mutate(xdist = case_when(is.na(xdist) ~ mean(xdist, na.rm = T), 
+                             TRUE ~ xdist),
+           ydist = case_when(is.na(ydist) ~ mean(ydist, na.rm = T), 
+                             TRUE ~ ydist))
+  #Saving data as csv file
+  write.csv(df, paste0(OutFolder, "csvFiles/", TagResults$name[1], ".csv"), row.names = F)
+  
+  ### Creating and exporting shapefile
+  #Creating shapefile with CRS: WGS84
+  coordinates(df) = ~lon+lat
+  proj4string(df) = CRS("++proj=longlat +datum=WGS84")
+  shapefile(df, paste0(OutFolder, TagResults$name[1], ".shp"), overwrite = T)
+  
+  ### Calculating distance between points in km
+  #Creating dataset which includes dates, coordinates and distances for each estimated
+  #location for that tag 
+  #distHaversine() provides distance in m, but they are divided by 1000 to present 
+  #distance in Km
+  distPts <- tibble(date = df@data$date, lon = df@coords[,"lon"], lat = df@coords[,"lat"],
+                   dist_km =  c(NA, round(distHaversine(df)/1000, 3)),
+                   vel_ms = dist_km*1000/86400)
+}
 
-##this converts those CIs into a lon and lat error metrics
-df <- cbind(res$tr, t(data.frame(lapply(bnds, FUN = function(x) c(x$yDist, x$xDist))))) %>% 
-  plyr::rename(c("1" = "ydist", "2" = "xdist")) %>% 
-  ##some contours will likely not calculate so you can fill this in with the mean error 
-  #(or something else) if you wish
-  mutate(xdist = case_when(is.na(xdist) ~ mean(xdist, na.rm = T), 
-                           TRUE ~ xdist),
-         ydist = case_when(is.na(ydist) ~ mean(ydist, na.rm = T), 
-                           TRUE ~ ydist))
 
-#save data in csv format for plotting in GIS software
-write.csv(df, "csvFiles/157567_idx2_bndNA_par2.25.csv", row.names = F)
-
-### Creating and exporting shapefile
-#Creating shapefile with CRS: WGS84
-coordinates(df) = ~lon+lat
-proj4string(df) = CRS("++proj=longlat +datum=WGS84")
-shapefile(df, "157567_idx2_bndNA_par2.25.shp", overwrite = T)
-
-### Calculating distance between points in km
-#Creating dataset which includes dates, coordinates and distances for each estimated
-#location for that tag 
-#distHaversine() provides distance in m, but they are divided by 1000 to present 
-#distance in Km
-distPts <- cbind(df@data$date, df@coords, dist_km =  c(NA, distHaversine(df)/1000))
 
 ###Plotting directly in R
 ## make a plot
